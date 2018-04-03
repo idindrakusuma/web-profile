f996c2cbd25e7bcc2fae0ab7665b3041850f2254
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-04-03 12:43:14 +0700

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,17 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 
-gulp.task('scripts', function () {
+gulp.task('custom-scripts', function () {
     // first thing, where is file?
     gulp.src('resources/js/*.js')
         // make to min
         .pipe(uglify())
         // destination of assets
         .pipe(gulp.dest('assets/js/'));
+});
+
+gulp.task('plugins-script',function(){
+    gulp.src('resources/js/plugins/main-plugins.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js/plugins'));
 });
